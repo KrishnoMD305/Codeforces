549771efce310f5469d0f23c07edc61d35327a28
Krishno Mondol <krishnomd60@gmail.com>
2025-02-21 08:30:52 +0600

    Update Solution.cpp

--- 1896 A. Jagged Swaps/Solution.cpp
+++ 1896 A. Jagged Swaps/Solution.cpp
@@ -1,3 +1,8 @@
+// 307072836                    21 Feb,2025, 08:25 AM           Krishno_MD                         77 ms                          0 KB
+
+// Here it is forbidden to select index 1. So for a successful sorting there need to be stay 1 at the 1st index. If stay then we can sort every element
+// But if there is no 1 in the first index there is no possibility to become the array sorted because for sorting at the 1st there need a 1.
+
 #include<bits/stdc++.h>
 using namespace std;
 //Krishno_MD_KUET_CSE
@@ -9,7 +14,7 @@ void solve(){
         v.push_back(a);
         r.push_back(a);
     }
-    sort(r.begin(),r.end());
+    sort(r.begin(),r.end()); // For selecting that the array is already sorted or not 
     if(r==v){
         cout<<"YES\n"; return;
     }
