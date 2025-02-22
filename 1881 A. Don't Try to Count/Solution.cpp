d3d9159008913cbd74112e47b16cdc1ef6cfad5a
Krishno Mondol <krishnomd60@gmail.com>
2025-02-22 09:05:43 +0600

    Update Solution.cpp

--- 1881 A. Don't Try to Count/Solution.cpp
+++ 1881 A. Don't Try to Count/Solution.cpp
@@ -1,3 +1,8 @@
+// 307214275         22 Feb,2025, 09:01 AM         Krishno_MD                124 ms             100 KB
+
+// It is just a work of finding substring. But there will never be more than 5 operations. Or else it is not possible
+// That's why the loop is 6 times. If we give more loop it will cause memory and time
+
 #include<bits/stdc++.h>
 using namespace std;
 //Krishno_MD_KUET_CSE
