5864838ef4aa68d82561d02c7f05a2e7c619e075
Krishno Mondol <krishnomd60@gmail.com>
2025-02-23 14:45:52 +0600

    Update Solution.cpp

--- 1858 A. Buttons/Solution.cpp
+++ 1858 A. Buttons/Solution.cpp
@@ -1,3 +1,8 @@
+// The common buttons need to press first. This will decide who will win.
+// After pressing the common if Anna win, then we have to see that if his own switch is bigger than Kate or not. If bigger then Anna win if small then kate win
+// Same for Kate.
+//Bexause at the end whose switch is bigger after pressing the common, she will win.
+
 #include<bits/stdc++.h>
 using namespace std;
 //Krishno_MD_KUET_CSE
@@ -13,10 +18,10 @@ class K{
         void solution(){
             ll a,b,c; cin>>a>>b>>c;
             bool anna=false, kat=false;
-            
+            // Checking after pressing common switches who is the winner
             if(c%2 == 0){kat=true;}
             else{anna=true;}
-            
+            // Checking at the end who is the winner
             if(anna){
                 if(a>=b){cout<<"First\n";}
                 else{cout<<"Second\n";}
