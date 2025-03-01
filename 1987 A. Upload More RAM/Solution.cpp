aa2918f5ae3a0bd0c19dae64da3c5856e159b395
Krishna Mondal <krishnomd60@gmail.com>
2025-03-01 20:26:28 +0600

    Create Solution.cpp

---
+++ 1987 A. Upload More RAM/Solution.cpp
@@ -0,0 +1,26 @@
+// 308516798                      01 Mar,2025, 08:21 PM                       Krishno_MD                           46 ms                            0 KB 
+
+#include<bits/stdc++.h>
+using namespace std;
+//Krishno_MD_KUET_CSE
+#define f(n) for(int i=0; i<n; i++)
+#define p(i,n) for(int i=0; i<n; i++)
+#define vec vector<int>
+#define pb push_back
+#define ll long long
+class K{
+    public:
+        void solution(){
+            int n,k; cin>>n>>k;
+            cout<<(n-1)*k+1<<"\n";
+        }
+};
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        K solve; 
+        solve.solution(); 
+    }
+    return 0;
+}
