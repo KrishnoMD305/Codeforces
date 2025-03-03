1ad1a133eb4ceee942b6b52f8dae382fc5926039
Krishna Mondal <krishnomd60@gmail.com>
2025-03-03 18:37:56 +0600

    Update Solution.cpp

--- 1814 A. Coins/Solution.cpp
+++ 1814 A. Coins/Solution.cpp
@@ -1,3 +1,13 @@
+// 308783621                   03 March,2025, 06:15 PM                   Krishno_MD                             108 ms                   100 KB
+
+/* (n-2x)%k==0  => (n-2x) mod k = 0  =>n-2x = 0 (mod k)  => n = 2x (mod k)    => n mod K = 2x 
+1. If n mod k == 0 then 2x = 0  => x = 0. So for the condition n%k==0 there is a solution. 
+2. A theory: if ax = b mod m then, gcd(a,m) divides b
+              if 2x = n mod k then gcd(2,k) divides n
+if k is odd then gcd(2,k) = 1 (Always) and n/1 is always true for all values of n. So for k is odd there is solution.
+if k is even then gcd(2,k) = 2 (always) and n/2 is true if n is even. So, for n is even there is solution if k is odd.   */
+
+
 #include<bits/stdc++.h>
 using namespace std;
 //Krishno_MD_KUET_CSE
