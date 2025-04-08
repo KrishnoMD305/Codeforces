a49c15f3b92eca9eb1cf5af112ed9bcd30028c15
Krishna Mondal <krishnomd60@gmail.com>
2025-04-08 09:05:49 +0600

    Create Pypy.py

---
+++ 1909 B. Make Almost Equal With Mod/Pypy.py
@@ -0,0 +1,16 @@
+# 314437649	      Apr/08/2025 08:56UTC+6	      Krishno_MD	      1909B - Make Almost Equal With Mod	      PyPy 3-64	      Accepted	      171 ms	      3400 KB
+
+# Krishno_MD_KUET_CSE
+
+t = int(input())
+
+for i in range(t):
+    n = int(input())
+    v = list(map(int,input().split()))
+    r = 2
+    while True:
+        m = {i%r for i in v}
+        if len(m)==2:
+            break
+        r *= 2
+    print(r)
